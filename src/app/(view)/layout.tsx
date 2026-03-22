6e28d380f56e3c9b89e27411283eb85691f87424
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2026-03-22 23:24:33 +0900

    fix: Header Suspense 적용으로 blocking 렌더링 문제 해결

--- src/app/(view)/layout.tsx
+++ src/app/(view)/layout.tsx
@@ -2,6 +2,7 @@ import { AppSidebar } from "@/components/app-sidebar";
 import Header from "@/components/common/Header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { StrictPropsWithChildren } from "@/types/common";
+import { Suspense } from "react";
 
 interface ViewLayoutProps extends StrictPropsWithChildren {
   modal: React.ReactNode;
@@ -11,13 +12,17 @@ export default function ViewLayout({ modal, children }: ViewLayoutProps) {
   return (
     <div className="w-full flex flex-col">
       {modal}
-      <Header />
-      <SidebarProvider>
-        <AppSidebar className="mt-15" />
-        <SidebarInset>
-          <div className="grow pt-15">{children}</div>
-        </SidebarInset>
-      </SidebarProvider>
+      <Suspense fallback={<div className="h-25 w-full bg-black" />}>
+        <Header />
+      </Suspense>
+      <Suspense>
+        <SidebarProvider>
+          <AppSidebar className="mt-15" />
+          <SidebarInset>
+            <div className="grow pt-15">{children}</div>
+          </SidebarInset>
+        </SidebarProvider>
+      </Suspense>
     </div>
   );
 }
